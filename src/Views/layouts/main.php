de368869c820d23960f73a8eda1783a66c9e150b
Nicolas Bortoluzzi <bortoluzzinicolas@gmail.com>
2021-12-11 15:59:03 -0300

    Change folder structure

--- src/Views/layouts/main.php
+++ src/Views/layouts/main.php
@@ -47,7 +47,7 @@
     </div>
 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="assets/scripts.js"></script>
+    <script src="public/scripts.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
